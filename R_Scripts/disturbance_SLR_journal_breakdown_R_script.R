db9df3dcfd624db69e61df910822255e7b26f9bc
Liam Langley <liam.langley@exeter.ac.uk>
2023-03-02 13:50:54 +0000

    R scripts to generate paper accumulation plots.

---
+++ R_Scripts/disturbance_SLR_journal_breakdown_R_script.R
@@ -0,0 +1,9 @@
+####Script to plot paper accumulation over time for the disturbance SLR
+####Liam Langley
+####Date created - 02/03/2023
+
+## load libraries
+
+library(tidyverse)
+library(ggplot2)
+library(here)
